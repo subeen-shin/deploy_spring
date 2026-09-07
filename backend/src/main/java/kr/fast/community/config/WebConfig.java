d6cf028c72d6467322ad48ee3f467216a5db5860
subeen-shin <subeen.shin@gmail.com>
2026-09-07 12:09:48 +0900

    Update WebConfig.java

--- backend/src/main/java/kr/fast/community/config/WebConfig.java
+++ backend/src/main/java/kr/fast/community/config/WebConfig.java
@@ -1,9 +1,11 @@
 package kr.fast.community.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 	@Value("${file.path}")
